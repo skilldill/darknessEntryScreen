41956d65909c97c59862686e485b811b4e1d1e46
utbanovag <utbanovag@it2g.ru>
2022-03-09 10:09:19 +0300

    add ios

--- App.js
+++ App.js
@@ -6,8 +6,8 @@ import { EntryCodeProvider } from './services';
 const App = () => {
   return (
     <EntryCodeProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#000'}}>
+        <StatusBar barStyle="default" />
         <Navigation />
       </SafeAreaView>
     </EntryCodeProvider>
